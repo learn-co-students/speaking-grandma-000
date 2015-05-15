6fbbf3dfad680e4f1c8326affbeac03ccfeb8a85
Rsenger <Rgermosen12@gmail.com>
2015-05-14 21:02:27 -0400

    GRANDMA YOU HEAR ME.

--- grandma.rb
+++ grandma.rb
@@ -8,3 +8,13 @@
 # and yells back
 
 # NO, NOT SINCE 1938!
+
+def speak_to_grandma(greeting)
+
+
+	if greeting != greeting.upcase
+		return "HUH?! SPEAK UP, SONNY!"
+	else
+		return "NO, NOT SINCE 1938!"
+	end
+end
