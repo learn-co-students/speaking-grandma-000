30be55b5e5cc6fc49a2ee646b774dbb2000d9761
sammarcus <sam.marcus@me.com>
2015-06-04 02:28:24 -0400

    grandma will die soon

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,9 @@
-# Write a speak_to_grandma method.
-
-# Whatever you say to grandma, she should respond with
-# HUH?! SPEAK UP, SONNY!
-# unless you shout it (type in all capitals).
-
-# If you shout, she can hear you (or at least she thinks so) 
-# and yells back
-
-# NO, NOT SINCE 1938!
+def speak_to_grandma(incoming_phrase)
+  if incoming_phrase  != incoming_phrase .upcase()
+    return "HUH?! SPEAK UP, SONNY!"
+  else
+    return "NO, NOT SINCE 1938!"
+  end
+end
+#Grandma will die one day. One day.
+#Tell me if you read this and I will give you a special surprise.
