27017de503753b057cbb736fee5ba644c2ded552
Lucas Fields <lqf2b8@gmail.com>
2015-08-23 05:49:36 +0000

    finished

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,16 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
+def speak_to_grandma(input)
+  orig_phrase = input
+  big_phrase = input.upcase
+  if orig_phrase == big_phrase
+    "NO, NOT SINCE 1938!"
+  else
+    "HUH?! SPEAK UP, SONNY!"
+  end
+end
